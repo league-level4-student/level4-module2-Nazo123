fb77b0e46dc9859ff7fa2e89b962f633e5b46dd9
Nazo123 <48037675+Nazo123@users.noreply.github.com>
2020-12-10 18:43:26 -0800

    Just 2 Left

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -1,5 +1,7 @@
 package StringMethods;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 import java.util.Arrays;
 import java.util.Base64;
 
@@ -32,13 +34,21 @@ public class StringMethods {
 
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		return null;
+		if(s1.length()>s2.length()) {
+			return s1;
+		}
+		return s2;
 	}
 
 	
 	// if String s contains the word "underscores", change all of the spaces to underscores
 	public static String formatSpaces(String s) {
-		return null;
+	if(s.contains("underscores")) {
+		s = s.replace(' ', '_');
+
+	}
+
+		return s;
 	}
 
 	
@@ -46,35 +56,103 @@ public class StringMethods {
 	// You cannot assume there are no extra spaces around the name, but you can
 	// assume there is only one space between the first and last name
 	public static String lineLeader(String s1, String s2, String s3) {
+		
 		return null;
 	}
 	
 	
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
-		return 0;
+		int value = 0;
+		char[] a = s.toCharArray();
+		for(int i = 0; i< s.length(); i++) {
+			if(Character.isDigit(a[i])) {
+				value += Integer.parseInt(a[i]+"");
+ 			}
+		}
+		/*char[] a = s.toCharArray();
+		for(int i = 0; i < a.length;i++) {
+		switch(""+a[i]) {
+		case "1": value++;
+		break;
+		case "2": value = value+2;	
+		break;
+		case "3": value = value+3;
+	    break;
+		case "4": value = value+4;
+	    break;
+		case "5": value = value+5;
+	    break;
+		case "6": value = value+6;
+	    break;
+		case "7": value = value+7;
+	    break;
+		case "8": value = value+8;
+	    break;
+		case "9": value = value+9;
+	    break;
+	    }
+		*/
+		
+
+		return value;
 	}
 	
 	
 	// Return the number of times String substring appears in String s
 	public static int substringCount(String s, String substring) {
-		return 0;
+	if(s.contains(substring)) {
+		char[] a = s.toCharArray();
+		char[] b = substring.toCharArray();
+		int correct = 0;
+		boolean c = false;
+		for(int i = 0; i < a.length; i++) {
+			c = true;
+			for(int g = 0; g < b.length; g++) {
+				
+				if(a[i+g] == b[g]) {
+			
+				}
+				else {
+					c = false;
+					break;
+				}
+			}
+			if(c) {
+				correct++;
+			}
+			}
+
+		return correct;
+	}
+	return 0;
 	}
 
 	// Call Utitilities.encrypt to encrypt String s
 	public static String encrypt(String s, char key) {
-		return null;
+
+		char[] g = s.toCharArray();
+		byte[] b =  new byte[g.length];
+		for(int i = 0; i < g.length;i++) {
+			 b[i] =  (byte) g[i]; 
+		}
+		byte a = (byte) key;
+	
+		return Utilities.encrypt(b, a) ;
 	}
 
 	// Call Utilities.decrypt to decrypt the cyphertext
 	public static String decrypt(String s, char key) {
-		return null;
+		byte a = (byte) key;
+		
+		return Utilities.decrypt(s, a);
 	}
 
 
 	// Return the number of words in String s that end with String substring
 	// You can assume there are no punctuation marks between words
 	public static int wordsEndsWithSubstring(String s, String substring) {
+		
 		return 0;
 	}
 	
@@ -83,7 +161,42 @@ public class StringMethods {
 	// of String substring and the final occurrence
 	// You can assume that substring will appear at least twice
 	public static int distance(String s, String substring) {
-		return 0;
+		char[] a = s.toCharArray();
+		char[] b = substring.toCharArray();
+		int first = -1;
+		int last = -1;
+		int counter = 0;
+System.out.println(a.length);
+System.out.println(s);
+		for(int i = 0; i <a.length-b.length+1;i++) {
+			System.out.println(i);
+	System.out.println("era");
+			for(int c = 0; c < b.length; c++) {
+				System.out.println(c);
+				if(a[i+c]==b[c]) {
+					counter++;
+				}
+				else {
+					counter = 0;
+					break;
+				}
+				if(counter == b.length) {
+					if(first == -1) {
+						first = i+b.length;
+					}
+					else {
+						last = i;
+					}
+					counter = 0;
+					break;
+				}
+			}
+		}
+		System.out.println(s);
+		System.out.println(substring);
+		System.out.println(first);
+		System.out.println(last);
+		return last-first;
 	}
 
 
@@ -91,6 +204,21 @@ public class StringMethods {
 	// palindromes are words or phrases are read the same forward as backward.
 	// HINT: ignore/remove all punctuation and spaces in the String
 	public static boolean palindrome(String s) {
+		s = s.toLowerCase();
+		s = s.replace(" ", "");
+		s = s.replace("?","");
+		s = s.replace(".", "");
+		s = s.replace("!", "");
+		s = s.replace(",", "");
+		s = s.replace("–", "");
+		s = s.replace(":", "");
+		char[] a = s.toCharArray();
+		
+		for(int i = 0; i < (a.length-1)/2;i++) {
+			if(a[i] != a[a.length-i-1]) {
+				return false;
+			}
+		}
 		return true;
 	}
 	
